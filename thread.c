42da8819d2fb024298987ad9ebbb48f0b30feec8
Kalrav <kalravparsana@gmail.com>
2016-03-15 23:15:00 +0530

    first change

--- thread.c
+++ thread.c
@@ -1,33 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/ioctl.h> 
 #include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<signal.h>
-#include<pthread.h>
+#include <unistd.h> 
+#include <sys/types.h> 
+#include<sys/socket.h> 
+#include<netinet/in.h> 
+#include<signal.h> 
+#include<pthread.h> 
+#include <wiringPi.h>
+#include<time.h>
 
-#include "gpio.h"
 
-pthread_t pt1,pt2,pt3;
+
+
+struct val{
+	int  fd_;
+	int pin_;
+	int ld_;
+};
+
+struct val *values_read[10],*values_write[10];   
+
+
+pthread_t pt1,pt2,pt3,pt4,pt_gpio_read[10],pt_gpio_write[10];
 pthread_mutex_t lock;
+
+time_t start[9],stop[9];
 
 int fd, c;
 //GPIO_USER_DATA gpio_data;
 
 
-int flag=0;
+int flag[10]={0,};
+int us[9]={0,};
 
 void *fun1();
 void *fun2();
 void *fun3();
+void *fun4();
+void *gpio_read(struct val *);
+void *gpio_write(struct val *);
 
 
-FILE *fpp;
- char id[11];
+
+
+FILE *fpp,*f_usage,*f_stat;
+char id[11];
 int sockfd = 0, n = 0,i;
 char recvBuff[1024];
 struct sockaddr_in serv_addr; 
@@ -36,53 +56,72 @@ char ip[15];
 
 int ERR=0;
 
-/* Set direction IN or OUT for GPIO */
-int gpio_set_direction(int fd, GPIO_USER_DATA *gpio_data)
-{
-	int ret;
 
-//	printf("%s\n", __func__);
+int gpio_pins_read[9]={18,6,13,19,26,12,16,20,21};
+int gpio_pins_write[9]={23,3,4,17,8,22,10,9,5};
 
-	ret = ioctl(fd, GPIO_SET_DIRECTION, gpio_data);
-        if (ret < 0)
-                printf("GPIO_SET_VALUE ioctl failed, %s\n", __func__);
+void incase_of_sig(){
+
+	digitalWrite(27,LOW);
+	f_usage=fopen("usage.txt","w");
+	fprintf(f_usage,"%d %d %d %d %d %d %d %d %d",us[0],us[1],us[2],us[3],us[4],us[5],us[6],us[7],us[8]);
+        fclose(f_usage);
+//	f_stat=fopen("stat.txt","w");
+//	fprintf(f_stat,"%d %d %d %d %d %d %d %d %d",flag[0],flag[1],flag[2],flag[3],flag[4],flag[5],flag[6],flag[7],flag[8]);
+//	fclose(f_stat);
+	exit(0);
+
 }
-
-/* Set value for GPIO */
-int gpio_set_value(int fd, GPIO_USER_DATA *gpio_data)
-{
-	int ret;
-
-//	printf("%s\n", __func__);
-
-	ret = ioctl(fd, GPIO_SET_VALUE, gpio_data);
-	if (ret < 0)
-                printf("GPIO_SET_VALUE ioctl failed, %s\n", __func__);
-}
-
-/* Get GPIO value */
-int gpio_get_value(int fd, GPIO_USER_DATA *gpio_data)
-{
-	int ret;
-
-        ret = ioctl(fd, GPIO_GET_VALUE, gpio_data);
-        if (ret < 0)
-                printf("GPIO_GET_VALUE ioctl failed, %s\n", __func__);
-}
-
 
 
 int main(int argc,char *argv[])
 {
-	
+	int i=0;	
 	//strcpy(ip,argv[1]);	
+	
+	 wiringPiSetupGpio();	
+	signal(SIGSEGV,incase_of_sig);
+	signal(SIGINT,incase_of_sig);
+	signal(SIGPIPE,incase_of_sig);
+	//struct val *value_read[10],*values_write[10];	
+	
+	f_stat=fopen("stat.txt","r");
+	fscanf(f_stat,"%d %d %d %d %d %d %d %d %d",&flag[0],&flag[1],&flag[2],&flag[3],&flag[4],&flag[5],&flag[6],&flag[7],&flag[8]);
+	fclose(f_stat);
 
+/*	for(i=0;i<10;i++)
+	{
+		values_read[i]=(struct val* )malloc(sizeof(struct val));
+		 values_write[i]=(struct val* )malloc(sizeof(struct val)); 	
+	}
+
+//	int gpio_pins_read[10]={18,6,13,19,26,12,16,20,21,14};
+//	int gpio_pins_write[10]={23,3,4,17,27,22,10,9,11,25};	
+
+
+	for(i=0;i<10;i++)
+	{
+		insert_values(values_read[i],gpio_pins_read[i],0,i);
+		insert_values(values_write[i],gpio_pins_write[i],0,i);
+	}
+
+*/
+	for(i=0;i<9;i++)
+
+	{
+		pinMode(gpio_pins_write[i], OUTPUT);     // Set regular LED as output
+    		pinMode(gpio_pins_read[i], INPUT);
+//		digitalWrite(gpio_pins_write[i],LOW);
+	}
+
+//		pinMode(27,OUTPUT);
+
+	
 	pthread_create(&pt1,NULL,fun1,NULL);
 	pthread_create(&pt2,NULL,fun2,NULL);	
 	pthread_create(&pt3,NULL,fun3,NULL);
-
+	pthread_create(&pt4,NULL,fun4,NULL);
 	pthread_exit(NULL);
-
 	return 0;
 }
 
@@ -90,9 +129,11 @@ int main(int argc,char *argv[])
 void *fun1()
 {
 
-	fpp=fopen("id.txt","r");
+	int ld=0;
+	fpp=fopen("/home/pi/id.txt","r");
 	fscanf(fpp,"%s",id);
-	printf("id - %s\n",id); 
+//	int us[9]={0,};
+//	printf("id - %s\n",id); 
 
 	
 	memset(recvBuff, '0',sizeof(recvBuff));
@@ -107,36 +148,83 @@ void *fun1()
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000); 
 
-    if(inet_pton(AF_INET,"192.168.1.123", &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET,"192.168.1.2", &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
         return 1;
-    } 
+    }\
 
     if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
        printf("\n Error : Connect Failed \n");
-       return 1;
+       incase_of_sig();
     } 
+
+
+	char data[25];	
+
+	write(sockfd,"SWITCHBOARD",12);
+
 	while(1) {
-	
+	strcpy(recvBuff,"");	
     read(sockfd, recvBuff,50);
 	
 	  if(strcmp(recvBuff,"") != 0)
 		{
-	 		
-			if(strstr(recvBuff,id) != NULL)
+			
+			printf("aa strlen avyu %d\n",strlen(recvBuff));
+			if(strstr(recvBuff,id) != NULL && strlen(recvBuff)==11 )
 				{
-					pthread_mutex_lock(&lock);
-					for(i=0;i<=strlen(recvBuff);i++)
-						recvBuff[i]=recvBuff[i+10];
-					if(flag==1) flag=0;
-					else if(flag==0) flag=1;
-					pthread_mutex_unlock(&lock);
-					printf("flag =  %d\n",flag);
-				}
+					if(recvBuff[10]=='R')
+					{
+						  sprintf(data,"%s%d%d%d%d%d%d%d%d%d",id,flag[0],flag[1],flag[2],flag[3],flag[4],flag[5],flag[6],flag[7],flag[8]);
+	 	                                  write(sockfd,data,strlen(data)+1);
+					}
+					else if(recvBuff[10]=='U')
+					{
+						f_usage=fopen("usage.txt","r");
+						fscanf(f_usage,"%d %d %d %d %d %d %d %d %d",&us[0],&us[1],&us[2],&us[3],&us[4],&us[5],&us[6],&us[7],&us[8]);
+						printf("aa us - %d %d\n",us[0],us[1]);
+						sprintf(data,"%s %d %d %d %d %d %d %d %d %d",id,us[0],us[1],us[2],us[3],us[4],us[5],us[6],us[7],us[8]);
+						write(sockfd,data,strlen(data)+1);
+						fclose(f_usage);	
+					}
+					else if(recvBuff[10]=='E')
+					{
+					 f_usage=fopen("usage.txt","w");
+					const char *textt="0 0 0 0 0 0 0 0 0";
+					fprintf(f_usage,"%s",textt);
+					printf("last E\n");
+					fclose(f_usage);
+					} 
+					else
+					{
+						pthread_mutex_lock(&lock);
+						ld=recvBuff[10]-48;
+
+						if(flag[ld]==1)
+						{	
+							stop[ld]=time(NULL);	
+							 flag[ld]=0;
+							f_usage=fopen("usage.txt","w");
+                                               	        fscanf(f_usage,"%d %d %d %d %d %d %d %d %d",&us[0],&us[1],&us[2],&us[3],&us[4],&us[5],&us[6],&us[7],&us[8]);
+						//	printf("aa previous us - %d\n",us[ld]);
+							us[ld]+=(stop[ld]-start[ld]);
+						//	printf("start :::: %d stop :::: %d us::::%d\n",start[ld],stop[ld],us[ld]);
+							fprintf(f_usage,"%d %d %d %d %d %d %d %d %d",us[0],us[1],us[2],us[3],us[4],us[5],us[6],us[7],us[8]);
+							fclose(f_usage);
+						}
+						else if(flag[ld]==0)
+						{	
+							start[ld]=time(NULL);	
+							 flag[ld]=1;
+						}
+						pthread_mutex_unlock(&lock);		
+						printf("flag[%d] =  %d\n",ld,flag[ld]);
+					}           
+				} 
 	fflush(stdout);
-  recvBuff[0]='\0';
+
         }
      
 }
@@ -152,76 +240,211 @@ void *fun1()
 
 void *fun2()
 {
-	fd = open("/dev/bcm-gpio", O_RDWR);
-	/* memset the structures */
-	GPIO_USER_DATA gpio_data;
+	int i;
 
-	memset(&gpio_data, 0, sizeof(gpio_data));
+	 for(i=0;i<9;i++)
+        {
+                values_read[i]=(struct val* )malloc(sizeof(struct val));
+                 values_write[i]=(struct val* )malloc(sizeof(struct val));      
+        }
 
-	while(1)
-	{	
-		pthread_mutex_lock(&lock);
-		gpio_data.gpio_num=5;
-		gpio_data.gpio_direction = 1;
-		gpio_data.gpio_value = 1;	
-		gpio_get_value(fd, &gpio_data);
-		printf("The output value of GPIO %u = %u\n", gpio_data.gpio_num,gpio_data.gpio_value);
-		usleep(10000);
-	
-		if(gpio_data.gpio_value==1)
-		{
-			if(flag==1) flag=0;
-			else if(flag==0)  flag=1;
-			printf("flag =  %d\n",flag);
-			 usleep(10000);
+        int gpio_pins_read[9]={18,6,13,19,26,12,16,20,21};
+        int gpio_pins_write[9]={23,3,4,17,8,22,10,9,5};   
 
-		
-		}
-		pthread_mutex_unlock(&lock);
-		sleep(1);
+
+        for(i=0;i<9;i++)
+        {
+                insert_values(values_read[i],fd,gpio_pins_read[i],i);
+                insert_values(values_write[i],fd,gpio_pins_write[i],i);
 	}
+	
+	for(i=0;i<9;i++)
+	{
+		pthread_create(&pt_gpio_read[i],NULL,gpio_read,values_read[i]);
+		pthread_create(&pt_gpio_write[i],NULL,gpio_write,values_write[i]);
+	}
+	
+//	pthread_create(&pt_gpio_write[0],NULL,gpio_write,values_write[0]);
+	pthread_exit(NULL);
+	
 	close(fd);
-
 }
+
 
 void *fun3()
 {
-	
-	 GPIO_USER_DATA gpio_data;
-	 memset(&gpio_data, 0, sizeof(gpio_data));
-
-	 gpio_data.gpio_num=20;
-         gpio_data.gpio_direction = 1;
-
+	sleep(2);
 	while(1)
-	{
-		if(flag==1)
 		{
-		//	printf("inside on\n");
-			pthread_mutex_lock(&lock);
-			printf("inside on\n");
-		//	gpio_data.gpio_num=19;
-		//	gpio_data.gpio_direction = 1;
-			gpio_data.gpio_value = 1;	
-			gpio_set_value(fd, &gpio_data);
-			pthread_mutex_unlock(&lock);
+			f_stat=fopen("stat.txt","w");
+		        fprintf(f_stat,"%d %d %d %d %d %d %d %d %d",flag[0],flag[1],flag[2],flag[3],flag[4],flag[5],flag[6],flag[7],flag[8]);
+        		fclose(f_stat);
+			sleep(1);
 		}
-		else
-		{
-		//	printf("inside off\n");
-			pthread_mutex_lock(&lock);
-			printf("inside off\n");
-
-		//	gpio_data.gpio_num=19;
-		//	gpio_data.gpio_direction = 1;
-			gpio_data.gpio_value = 0;	
-			gpio_set_value(fd, &gpio_data);
-			pthread_mutex_unlock(&lock);	
-		}
-	}
 }
 	
 
 
 
 
+void *fun4()
+{
+	char rcbuff[100]="";
+	int sockfdd;
+
+//		printf("In while\n");
+	    if((sockfdd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	    {
+	        printf("\n Error : Could not create socket \n");
+        	incase_of_sig();
+    	    } 
+		
+	struct timeval tv;	
+	tv.tv_sec=2;
+	if(setsockopt(sockfdd,SOL_SOCKET,SO_RCVTIMEO,(struct timeval *)&tv,sizeof(struct timeval))) {
+		incase_of_sig(); }
+
+    		memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+    		serv_addr.sin_family = AF_INET;
+    		serv_addr.sin_port = htons(5000); 
+
+  	  if(inet_pton(AF_INET,"192.168.1.2", &serv_addr.sin_addr)<=0)
+    		{
+       			 printf("\n inet_pton error occured\n");
+        		incase_of_sig();
+    		}
+
+   	 if( connect(sockfdd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    		{
+       			printf("\n Error : Connect Failed \n");
+       			incase_of_sig();
+   		 }
+			pinMode(27,OUTPUT);
+			digitalWrite(27,HIGH); 
+	write (sockfdd,"READERGROUP",12);usleep(100);
+	read(sockfdd,rcbuff,100);usleep(100);
+//	pinMode(27,OUTPUT);
+	while(1)
+	{
+//		write(sockfd,"HEARTBEATCK",12);
+		if(send(sockfdd,"HEARTBEATCK",12,MSG_NOSIGNAL) <= 0 )incase_of_sig();
+		usleep(100);
+//		read(sockfdd,rcbuff,100);
+//		printf("-- %s --" , rcbuff);	
+//		if(strcmp(rcbuff,"HEARTBEATCK")==0)
+		if(read(sockfdd,rcbuff,100)>0)
+		{
+			digitalWrite(27,HIGH);
+	//			printf("In yes");
+		}
+		else
+		
+		{
+			digitalWrite(27,LOW);
+	//		printf("In no");
+			incase_of_sig();
+  		}
+		sleep(1);	
+	}
+//	if(write(sockfdd,"RCVQUITMESG",12)<0) incase_of_sig();
+						
+//	close(sockfdd);
+}	
+
+
+
+
+
+void *gpio_read(struct val *temp)
+{
+
+//	 GPIO_USER_DATA gpio_data; 
+
+    //     memset(&gpio_data, 0, sizeof(gpio_data));
+
+
+//	 gpio_data.gpio_num=temp->pin_;
+  //       gpio_data.gpio_direction = 1;
+
+
+	while(1)
+	{
+			// GPIO_USER_DATA gpio_data; 
+
+                       // memset(&gpio_data, 0, sizeof(gpio_data));
+      
+                pthread_mutex_lock(&lock);
+        //        gpio_data.gpio_num=temp->pin_;
+         //       gpio_data.gpio_direction = 1;
+  //              gpio_data.gpio_value = 1;       
+                
+              //  printf("The output value of GPIO %u = %u\n", gpio_data.gpio_num,gpio_data.gpio_value);
+                usleep(10000);
+
+                if(digitalRead(temp->pin_))
+                {
+                        if(flag[temp->ld_]==1) flag[temp->ld_]=0;
+                        else if(flag[temp->ld_]==0)  flag[temp->ld_]=1;
+                  //	 printf("flag =  %d\n",flag[temp->ld_]);
+                         usleep(10000);
+
+
+                }
+                pthread_mutex_unlock(&lock);
+               sleep(1);
+	}
+}
+
+
+
+void *gpio_write(struct val *temp)
+{
+//	  GPIO_USER_DATA gpio_data;
+  //       memset(&gpio_data, 0, sizeof(gpio_data));
+
+    //    gpio_data.gpio_num=temp->pin_;
+      //  gpio_data.gpio_direction = 1;
+
+	while(1)
+	{
+
+			if(flag[temp->ld_]==1)
+        	        {
+      //          	      printf("inside on\n");
+                        	pthread_mutex_lock(&lock);
+               		 //        printf("inside on\n");
+               		 //      gpio_data.gpio_num=temp->pin_;
+               		 //      gpio_data.gpio_direction = 1;
+//                       		 gpio_data.gpio_value = 1;       
+                       	//	 gpio_set_value(fd, &gpio_data);
+				digitalWrite(temp->pin_, HIGH);
+                       		 pthread_mutex_unlock(&lock);
+			 	 usleep(1000);
+               		 }
+               		 else
+               		 {
+        //       		       printf("inside off\n");
+                       		 pthread_mutex_lock(&lock);
+               		   //      printf("inside off\n");
+
+               		   //    gpio_data.gpio_num=temp->pin_;
+                	   //   gpio_data.gpio_direction = 1;
+                       		// gpio_data.gpio_value = 0;       
+                       		 //gpio_set_value(fd, &gpio_data);
+				digitalWrite(temp->pin_,LOW);
+                        	pthread_mutex_unlock(&lock);
+				usleep(1000);    
+              		  }
+			
+	}
+}
+
+
+
+void insert_values(struct val *ptr,int fds,int pins,int lds)
+{
+	ptr->fd_=fds;
+	ptr->pin_=pins;
+	ptr->ld_=lds;
+}
