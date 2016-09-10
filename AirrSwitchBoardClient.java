85a326fa365b6f73ad205111ddf1ec61df15abb2
Kalrav Parsana <kalravparsana@gmail.com>
2016-09-10 23:45:01 +0530

    SwitchBoard Client major improvements 	-usage support 	-stat support 	-back to previous stat support

--- AirrSwitchBoardClient.java
+++ AirrSwitchBoardClient.java
@@ -1,6 +1,11 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.Socket;
@@ -10,6 +15,10 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 class AirrSwitchBoardClient implements Runnable
 {
@@ -27,11 +36,21 @@ class AirrSwitchBoardClient implements Runnable
     private static String myId;
     private static boolean isSystemArmed = false;
     static List<PlugHandler> plugs = new ArrayList<PlugHandler>();
+    private final ExecutorService executor = Executors.newFixedThreadPool(1);
 
     Thread t = null;
+    Thread usageUpdater = null;
 
     DataOutputStream dout;
     DataInputStream din;
+
+    Properties prop = new Properties();
+    Properties UsageProp = new Properties();
+    OutputStream output = null;
+    InputStream input = null;
+
+    OutputStream UsageOutput = null;
+    InputStream UsageInput = null;
 
     AirrSwitchBoardClient() throws Exception
     {
@@ -67,6 +86,36 @@ class AirrSwitchBoardClient implements Runnable
             plugs.add(plugHandler);
         }
 
+        try
+        {
+            File f = new File("stat.properties");
+            if (!f.exists())
+            {
+                output = new FileOutputStream("stat.properties");
+                for (int iCount = 0; iCount < 100; iCount++)
+                {
+                    prop.setProperty("plug".concat(Integer.toString(iCount)), "false");
+                }
+                prop.store(output, null);
+                output.close();
+            }
+            else
+            {
+                input = new FileInputStream("stat.properties");
+                prop.load(input);
+                for (int iCount = 0; iCount < 100; iCount++)
+                {
+                    Boolean stat = Boolean.parseBoolean(prop.getProperty("plug".concat(Integer.toString(iCount))));
+                    plugs.get(iCount).setIsOn(stat);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        executor.submit(new UsageUpdater());
     }
 
     public static void main(String args[]) throws Exception
@@ -123,6 +172,34 @@ class AirrSwitchBoardClient implements Runnable
                             }
                         }
                         dout.writeUTF(status);
+                    }
+                    else if (query.equals("USAGEREPORT"))
+                    {
+                        StringBuilder usageStr = new StringBuilder("");
+                        for (int iCount = 0; iCount < 100; iCount++)
+                        {
+                            int usage = Integer.parseInt(UsageProp.getProperty("plug".concat(Integer.toString(iCount))));
+                            usageStr.append(Integer.toString(usage));
+                            usageStr.append(" ");
+                        }
+
+                        dout.writeUTF(usageStr.toString());
+                    }
+                    else if (query.equals("RESETUSAGE"))
+                    {
+                        try
+                        {
+                            executor.shutdown();
+                            File f = new File("usage.properties");
+                            f.delete();
+                            executor.submit(new UsageUpdater());
+                        }
+                        catch (Exception e)
+                        {
+
+                        }
+
+                        dout.writeUTF("USAGERESETED");
                     }
                     else
                     {
@@ -227,18 +304,25 @@ class AirrSwitchBoardClient implements Runnable
         private boolean isOn = false;
         private boolean previosState = false;
 
+        public void setIsOn(boolean value)
+        {
+            isOn = value;
+        }
+
         public void setOnOff(boolean value)
         {
             isOn = value;
             if (isOn)
             {
-                new RunCommand("eject").start();
+                new RunCommand("echo ").start();
                 System.out.println(plugNumber + " Turned on");
             }
             else
             {
                 System.out.println(plugNumber + " Turned off");
             }
+
+            new UpdateProperty(isOn,plugNumber).start();
         }
 
         public boolean isOn()
@@ -281,5 +365,95 @@ class AirrSwitchBoardClient implements Runnable
         {
             command = arg;
         }
+    }
+
+    private class UpdateProperty extends Thread
+    {
+        private boolean stat;
+        private int plugNumber;
+
+        UpdateProperty(Boolean value,int i)
+        {
+            stat = value;
+            plugNumber = i;
+        }
+
+        public void run()
+        {
+            try
+            {
+                Boolean currentStat = Boolean.parseBoolean(prop.getProperty("plug".concat(Integer.toString(plugNumber))));
+                if (stat != currentStat)
+                {
+                    FileOutputStream out = new FileOutputStream("stat.properties");
+                    prop.setProperty("plug".concat(Integer.toString(plugNumber)), Boolean.toString(stat));
+                    prop.store(out, null);
+                    out.close();
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+
+    private class UsageUpdater implements Runnable
+    {
+
+        UsageUpdater()
+        {
+            try
+            {
+                File f = new File("usage.properties");
+                if (!f.exists())
+                {
+                    UsageOutput = new FileOutputStream("usage.properties");
+                    for (int iCount = 0; iCount < 100; iCount++)
+                    {
+                        UsageProp.setProperty("plug".concat(Integer.toString(iCount)), "0");
+                    }
+                    UsageProp.store(UsageOutput, null);
+                    UsageOutput.close();
+                }
+                else
+                {
+                    UsageInput = new FileInputStream("usage.properties");
+                    UsageProp.load(UsageInput);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        public void run()
+        {
+            while (true)
+            {
+                try
+                {
+                    FileOutputStream out = new FileOutputStream("usage.properties");
+
+                    for (int iCount = 0; iCount < 100; iCount++)
+                    {
+                        if(plugs.get(iCount).isOn())
+                        {
+                            int usage = Integer.parseInt(UsageProp.getProperty("plug".concat(Integer.toString(iCount))));
+                            UsageProp.setProperty("plug".concat(Integer.toString(iCount)), Integer.toString(usage + 1));
+                        }
+                    }
+                    UsageProp.store(out, null);
+                    out.close();
+                    Thread.sleep(1000);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+
+        }
+
     }
 }
