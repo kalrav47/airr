c8edd0ab3a65e4ca9b74c3fc05457eda3a695122
Kalrav Parsana <kalravparsana@gmail.com>
2016-11-20 23:43:21 +0530

    Provided algorithm to generate PAN ssid and password

--- ap_config/install_ap_conf.sh
+++ ap_config/install_ap_conf.sh
@@ -3,6 +3,10 @@ cp access_point.conf /etc/dnsmasq.d/access_point.conf
 cp interfaces /etc/network/interfaces
 sudo ifup wlan1
 sudo /etc/init.d/dnsmasq restart
+ssid=`echo $1 | sha256sum | base64 | head -c 58`
+password=`echo $1 | sha384sum | base64 | tail -c 61 | head -c 58`
+sed -i "3s/kalrav_from_chip/"${ssid}"/" hostapd.conf
+sed -i "9s/123456789/"${password}"/" hostapd.conf
 cp hostapd.conf /etc/hostapd.conf
 cp hostapd-systemd.service /lib/systemd/system/hostapd-systemd.service
 sudo update-rc.d hostapd disable
