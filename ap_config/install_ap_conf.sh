5ea2fc83b4655af9804913ffd21c64aa53d8961c
Kalrav Parsana <kalravparsana@gmail.com>
2016-11-27 13:37:37 +0530

    Bug fixing in server provisioning script

--- ap_config/install_ap_conf.sh
+++ ap_config/install_ap_conf.sh
@@ -3,16 +3,14 @@ cp access_point.conf /etc/dnsmasq.d/access_point.conf
 cp interfaces /etc/network/interfaces
 sudo ifup wlan1
 sudo /etc/init.d/dnsmasq restart
-ssid=`echo $1 | sha256sum | base64 | head -c 58`
+ssid=`echo $1 | sha256sum | base64 | head -c 16`
 password=`echo $1 | sha384sum | base64 | tail -c 61 | head -c 58`
 sed -i "3s/airr/"${ssid}"/" hostapd.conf
 sed -i "9s/123456789/"${password}"/" hostapd.conf
 cp hostapd.conf /etc/hostapd.conf
-cp hostapd-systemd.service /lib/systemd/system/hostapd-systemd.service
-sudo update-rc.d hostapd disable
-sudo systemctl daemon-reload
-sudo systemctl enable hostapd-systemd
+cp startapmode.sh /root/
+chmod +x /root/startapmode.sh
 crontab -l > mycron
-echo "@reboot sudo systemctl start hostapd-systemd" >> mycron
+echo "@reboot /root/startapmode.sh" >> mycron
 crontab mycron
 rm mycron
--- server_provisioning.sh
+++ server_provisioning.sh
@@ -4,13 +4,41 @@ chmod +x install_ap_conf.sh
 sh install_ap_conf.sh $1
 cd ../
 
+sync
+sync
+sync
+
+#copy files
+cd executables
+cp server ~/
+chmod +x ~/server
+cp server.sh ~/
+chmod +x ~/server.sh
+cd ../
+crontab -l > mycron
+echo "@reboot /root/server.sh" >> mycron
+crontab mycron
+rm mycron
+
+sync
+sync
+sync
+
 #disable uart
 systemctl mask serial-getty@ttyS0.service
 systemctl stop serial-getty@ttyS0.service
 systemctl disable serial-getty@ttyS0.service
 
+sync
+sync
+sync
+
 #disable ssh
 apt-get purge openssh-server
+
+sync
+sync
+sync
 
 #disable login from screen
 systemctl disable serial-getty@ttyGS0.service
@@ -18,6 +46,14 @@ systemctl disable serial-getty@ttyGS0.service
 #disable bluetooth
 systemctl disable bluetooth.service
 
+sync
+sync
+sync
+
 #change root password
 root_password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 echo "root:"${root_password}"" | chgpasswd
+sync
+sync
+sync
+
