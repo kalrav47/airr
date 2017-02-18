3c0e79c17b9d2d180be1b3e1b3ddb23271e341ce
Kalrav Parsana <kalravparsana@gmail.com>
2017-02-19 00:56:14 +0530

    added connman wifi connect script

---
+++ wifi_config/wifi_connect.sh
@@ -0,0 +1,27 @@
+while true;do
+ping -c 1 google.com >/dev/null 2>&1
+online=$?
+if [ $online -eq 0 ]; then
+    echo "Online"
+else
+        echo "Offline"
+        connmanctl enable wifi > /dev/null 2>&1
+        connmanctl scan wifi > /dev/null 2>&1
+        ssid=`connmanctl services | grep $1 | tail -1 | grep -oE '[^ ]+$'`
+        #echo $ssid
+
+        #for i in $(echo $ssid | sed "s/_/ /g")
+        #do
+        # call your procedure/other scripts here below
+        #       echo "$i"
+        #done
+
+        echo "[service_$ssid]" > /var/lib/connman/$1-psk.config
+        echo "Type = wifi" >> /var/lib/connman/$1-psk.config
+        echo "Name = $1" >> /var/lib/connman/$1-psk.config
+        echo "Passphrase = $2" >> /var/lib/connman/$1-psk.config
+        sleep 25
+fi
+sleep 5
+done
+
